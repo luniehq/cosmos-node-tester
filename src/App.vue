352d1179c77de6df7ea9d9ce39cf035db1ae5f25
FaboWeb <frznhope@gmail.com>
2020-01-31 20:18:54 -0500

    some comments

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="app">
+    Lunie requires nodes to provide the Cosmos SDK REST API to read the chain state. The API needs to be available via https.
+    <br />Lunie requires a Tendermint Websocket connection to react to on chain events (like a new block). The websocket needs to be available via wss.
+    <br />On this page you can test the 2 endpoints.
+    <br />
     <input v-model="restURL" placeholder="enter REST Url..." />
     <br />
     <input v-model="tendermintUrl" placeholder="enter Tendermint Url..." />
@@ -8,15 +12,15 @@
     <br />
     <span v-if="success">The provided endpoints are compatible to be used by Lunie</span>
     <br />
-    <span v-if="restError">Cosmos SDK REST API is not reachable</span>
+    <span v-if="restError">Cosmos SDK REST API is not reachable: {{ restError }}</span>
     <br />
-    <span v-if="tendermintError">Tendermint Websockets are not reachable</span>
+    <span v-if="tendermintError">Tendermint Websockets are not reachable: {{ tendermintError }}</span>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
   data: () => ({
     restURL: undefined,
     tendermintUrl: undefined,
@@ -26,38 +30,38 @@ export default {
     loading: false
   }),
   methods: {
-    async check () {
-      this.tendermintError = false
-      this.restError = false
-      this.success = false
+    async check() {
+      this.tendermintError = undefined;
+      this.restError = undefined;
+      this.success = false;
 
-      this.loading = true
+      this.loading = true;
 
       const responses = await Promise.all([
         new Promise((resolve, reject) => {
-          const socket = new WebSocket(this.tendermintUrl)
-          socket.onopen = resolve
-          socket.onclose = reject
+          const socket = new WebSocket(this.tendermintUrl);
+          socket.onopen = resolve;
+          socket.onclose = reject;
         })
           .then(() => true)
-          .catch(() => {
-            this.tendermintError = true
-            return false
+          .catch(error => {
+            this.tendermintError = error;
+            return false;
           }),
-        fetch(`${this.restURL.trim('/')}/staking/validators`)
+        fetch(`${this.restURL.trim("/")}/staking/validators`)
           .then(res => res.json())
           .then(() => true)
-          .catch(() => {
-            this.restError = true
-            return false
+          .catch(error => {
+            this.restError = error;
+            return false;
           })
-      ])
+      ]);
 
       // success if non failed
-      this.success = responses[0] && responses[1]
+      this.success = responses[0] && responses[1];
     }
   }
-}
+};
 </script>
 
 <style>
